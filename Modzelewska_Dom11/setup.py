f1af9515138ae76dfa490d258ac5391076bca5e5
agattt <a.modzelewsk12@student.uw.edu.pl>
2022-01-23 19:03:43 +0100

    Deleted Packaging

--- Modzelewska_Dom11/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="homework_exercise",
-    version="0.1",
-    packages=["homework_exercise"],
-    package_data={"": ["*.json", "*.toml"]},
-    long_description='Readme.rst'
-)
